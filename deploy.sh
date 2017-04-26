f1a98d9ba860e3a4d8d51a1ff427fca473e5c7cb
Tarikur Rahaman <sohel2020@gmail.com>
2017-04-26 20:49:24 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 # Edit these constants
-APP_NAME="app"
-APP_CONTAINERS_NAME="docker_app"
+APP_NAME="tonic-core"
+
 NUM_OF_APP_CONTAINERS=2
-DEPLOYMENT_TIME=10
+DEPLOYMENT_TIME=15
+DIR=${PWD##*/}
+C_DIR="${DIR//-}"
+P_GREP=$C_DIR$APP_NAME"_"
 # End of edit
 
-docker-compose pull $APP_NAME # Use (docker-compose build $APP_NAME if you use a custom image) 
+docker-compose build $APP_NAME # Use (docker-compose build $APP_NAME if you use a custom image)
 docker-compose scale $APP_NAME=$(($NUM_OF_APP_CONTAINERS*2))
-
 sleep $DEPLOYMENT_TIME
+echo $P_GREP
 
-FIRST_APP_CONTAINER_NUM=`docker inspect --format='{{.Name}}' $(docker ps -q) | grep $APP_CONTAINERS_NAME | awk -F  "_" '{print $NF}' | sort | head -1`
+FIRST_APP_CONTAINER_NUM=`docker inspect --format='{{.Name}}' $(docker ps -q) | grep $P_GREP | awk -F  "_" '{print $NF}' | sort -n | head -2`
+CONTAINER_COUNT=`docker inspect --format='{{.Name}}' $(docker ps -q) | grep $P_GREP | awk -F  "_" '{print $NF}' | sort -n | head -2 | wc -l`
 
-for ((i=$FIRST_APP_CONTAINER_NUM;i<$FIRST_APP_CONTAINER_NUM+$NUM_OF_APP_CONTAINERS;i++))
+T_NUM=$(($CONTAINER_COUNT+$NUM_OF_APP_CONTAINERS))
+echo "T_NUM"
+echo $T_NUM
+echo "FIRST_APP_CONTAINER_NUM"
+echo $FIRST_APP_CONTAINER_NUM
+echo "NUM_OF_APP_CONTAINERS"
+echo $NUM_OF_APP_CONTAINERS
+
+for i in $FIRST_APP_CONTAINER_NUM
+#for ((i=$FIRST_APP_CONTAINER_NUM;i<$T_NUM;i++))
 do
-   docker stop "$APP_CONTAINERS_NAME"_"$i"
-   docker rm "$APP_CONTAINERS_NAME"_"$i"
+echo $i
+  docker stop "$C_DIR"_"$APP_NAME"_"$i"
+  docker rm "$C_DIR"_"$APP_NAME"_"$i"
 done
